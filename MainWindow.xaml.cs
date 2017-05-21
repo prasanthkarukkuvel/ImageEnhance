c6c36ea6de229ede7bca261f25ece5ab81efe31e
Prasanth <prashanth702@gmail.com>
2017-05-21 19:09:53 +0530

    Stage 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using Microsoft.Win32;
 using System.Threading;
+using System.Runtime;
 using System.Diagnostics;
 
 namespace ImageEnhance
@@ -28,7 +29,7 @@ namespace ImageEnhance
         }
 
         public BitmapImage Image { get; set; }
-        public Int32Rect ImageRect => Image != null ? new Int32Rect(0, 0, (int)Image.PixelWidth, (int)Image.PixelHeight) : Int32Rect.Empty;
+        public Int32Rect ImageRect => Image != null ? new Int32Rect(0, 0, Image.PixelWidth, Image.PixelHeight) : Int32Rect.Empty;
 
         private void Open_Click(object sender, RoutedEventArgs e)
         {
@@ -40,47 +41,62 @@ namespace ImageEnhance
                 Previewer.Source = Image;
             }
         }
-        private void Adjuster_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            Previewer.Source = ChangeContrast(Adjuster.Value);
-        }
 
-        private ImageSource ChangeContrast(double AdjustValue)
+        private void Adjuster_LostMouseCapture(object sender, MouseEventArgs e)
         {
             if (Image != null)
             {
-                byte[] Pixels = GetPixles(Image, ImageRect);
-                double ContrastLevel = Math.Pow((100.0 + AdjustValue) / 100.0, 2);
+                Adjuster.IsEnabled = false;
+                var Value = Adjuster.Value;
+                ProcessPixles(Value);
+            }
+        }
 
-                for (int k = 0; k + 4 < Pixels.Length; k += 4)
-                {                  
-                    Pixels[k] = CorrectContrast(GetContrast(Pixels[k], ContrastLevel));
-                    Pixels[k + 1] = CorrectContrast(GetContrast(Pixels[k + 1], ContrastLevel));
-                    Pixels[k + 2] = CorrectContrast(GetContrast(Pixels[k + 2], ContrastLevel));
-                }
+        private void ProcessPixles(double Value)
+        {
+            var Stride = Image.PixelWidth * ((Image.Format.BitsPerPixel + 7) / 8);
+            byte[] Pixels = new byte[(int)Image.Height * Stride];
+            Image.CopyPixels(ImageRect, Pixels, Stride, 0);
 
-                return SetPixles(Image, ImageRect, Pixels);
+            new Thread(() => InvokeChangeContrast(Pixels, Value, HandleChangeContrast))
+                    .Start();
+        }
+
+        private static void InvokeChangeContrast(byte[] Pixels, double AdjustValue, Action<byte[]> Callback)
+        {
+            Callback(ChangeContrast(Pixels, AdjustValue));
+        }
+
+        private void HandleChangeContrast(byte[] Source)
+        {
+            Dispatcher.BeginInvoke((Action)(() =>
+             {               
+                 Previewer.Source = SetPixles(Source); 
+                 Adjuster.IsEnabled = true;
+             }));
+        }
+
+        private ImageSource SetPixles(byte[] Pixels)
+        {
+            var Source = new WriteableBitmap(Image);
+            Source.WritePixels(ImageRect, Pixels, (Image.PixelWidth * ((Image.Format.BitsPerPixel + 7) / 8)), 0);
+            return Source;
+        }
+
+        private static byte[] ChangeContrast(byte[] Pixels, double AdjustValue)
+        {
+            double ContrastLevel = Math.Pow((100.0 + AdjustValue) / 100.0, 2);
+
+            for (int k = 0; k + 4 < Pixels.Length; k += 4)
+            {
+                Pixels[k] = CorrectContrast(GetContrast(Pixels[k], ContrastLevel));
+                Pixels[k + 1] = CorrectContrast(GetContrast(Pixels[k + 1], ContrastLevel));
+                Pixels[k + 2] = CorrectContrast(GetContrast(Pixels[k + 2], ContrastLevel));
             }
 
-            return null;
+            return Pixels;
         }
         private static double GetContrast(byte Pixel, double ContrastLevel) => ((((Pixel / 255.0) - 0.5) * ContrastLevel) + 0.5) * 255.0;
         private static byte CorrectContrast(double Color) => (byte)(Color > 255 ? 255 : Color < 0 ? 0 : Color);
-
-        private static byte[] GetPixles(BitmapImage Image, Int32Rect ImageRect)
-        {
-            var Source = new WriteableBitmap(Image);
-            var Stride = Source.PixelWidth * ((Source.Format.BitsPerPixel + 7) / 8); 
-
-            byte[] Pixels = new byte[(int)Source.Height * Stride];
-            Source.CopyPixels(ImageRect, Pixels, Stride, 0);
-            return Pixels;
-        }
-        private static ImageSource SetPixles(BitmapImage Image, Int32Rect ImageRect, byte[] Pixels)
-        {
-            var Source = new WriteableBitmap(Image);
-            Source.WritePixels(ImageRect, Pixels, (Source.PixelWidth * ((Source.Format.BitsPerPixel + 7) / 8)), 0);
-            return Source;
-        }
     }
 }
