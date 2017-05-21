09a6254d5306c4f0ac0763ada6aa856fbf803e11
Prasanth <prashanth702@gmail.com>
2017-05-22 00:46:53 +0530

    Stage 3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using Microsoft.Win32;
 using System.Threading;
+using System.IO;
 using System.Runtime;
 using System.Diagnostics;
 
@@ -27,21 +28,28 @@ namespace ImageEnhance
         {
             InitializeComponent();
         }
-
+        public string FileName { get; set; }
         public BitmapImage Image { get; set; }
         public Int32Rect ImageRect => Image != null ? new Int32Rect(0, 0, Image.PixelWidth, Image.PixelHeight) : Int32Rect.Empty;
-
+        public int Counter { get; set; }
         private void Open_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog Dialog = new OpenFileDialog();
             Dialog.Filter = "JPEG|*.jpg;";
             if ((bool)Dialog.ShowDialog(this))
             {
-                Image = new BitmapImage(new Uri(Dialog.FileName));
+                FileName = Dialog.FileName;
+                using (var Stream = File.Open(FileName, FileMode.Open, FileAccess.Read))
+                {
+                    Image = new BitmapImage();
+                    Image.BeginInit();
+                    Image.CacheOption = BitmapCacheOption.OnLoad;
+                    Image.StreamSource = Stream;
+                    Image.EndInit();
+                }
                 Previewer.Source = Image;
             }
         }
-
         private void Adjuster_LostMouseCapture(object sender, MouseEventArgs e)
         {
             if (Image != null)
@@ -51,38 +59,53 @@ namespace ImageEnhance
                 ProcessPixles(Value);
             }
         }
-
         private void ProcessPixles(double Value)
         {
-            var Stride = Image.PixelWidth * ((Image.Format.BitsPerPixel + 7) / 8);
-            byte[] Pixels = new byte[(int)Image.Height * Stride];
-            Image.CopyPixels(ImageRect, Pixels, Stride, 0);
+            var Source = new WriteableBitmap(Image);
+            var Width = Image.PixelWidth / 4;
+            var Height = Image.PixelHeight / 2;
+            var Stride = Width * ((Image.Format.BitsPerPixel + 7) / 8);
+            var Size = Height * Stride;
+            Counter = 4 * 2;
 
-            new Thread(() => InvokeChangeContrast(Pixels, Value, HandleChangeContrast))
-                    .Start();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    byte[] Pixels = new byte[Size];
+
+                    var Rect = new Int32Rect(i * Width, j * Height, Width, Height);
+
+                    Image.CopyPixels(Rect, Pixels, Stride, 0);
+
+                    new Thread(() => InvokeChangeContrast(Pixels, Value, Rect, Stride, Source, HandleChangeContrast))
+                       .Start();
+                }               
+            }
         }
-
-        private static void InvokeChangeContrast(byte[] Pixels, double AdjustValue, Action<byte[]> Callback)
+        private static void InvokeChangeContrast(byte[] Pixels, double AdjustValue, Int32Rect Rect, int Stride, WriteableBitmap Source, Action<byte[], Int32Rect, int, WriteableBitmap> Callback)
         {
-            Callback(ChangeContrast(Pixels, AdjustValue));
+            Callback(ChangeContrast(Pixels, AdjustValue), Rect, Stride, Source);
         }
-
-        private void HandleChangeContrast(byte[] Source)
+        private void HandleChangeContrast(byte[] Pixels, Int32Rect Rect, int Stride, WriteableBitmap Source)
         {
             Dispatcher.BeginInvoke((Action)(() =>
-             {               
-                 Previewer.Source = SetPixles(Source); 
-                 Adjuster.IsEnabled = true;
+             {
+                 Counter--;
+
+                 SetPixles(Pixels, Rect, Stride, Source);
+
+                 if (Counter == 0)
+                 {
+                     Previewer.Source = Source;
+                     Adjuster.IsEnabled = true;
+                 }
              }));
         }
-
-        private ImageSource SetPixles(byte[] Pixels)
+        private void SetPixles(byte[] Pixels, Int32Rect Rect, int Stride, WriteableBitmap Source)
         {
-            var Source = new WriteableBitmap(Image);
-            Source.WritePixels(ImageRect, Pixels, (Image.PixelWidth * ((Image.Format.BitsPerPixel + 7) / 8)), 0);
-            return Source;
+            Source.WritePixels(Rect, Pixels, Stride, 0);
         }
-
         private static byte[] ChangeContrast(byte[] Pixels, double AdjustValue)
         {
             double ContrastLevel = Math.Pow((100.0 + AdjustValue) / 100.0, 2);
